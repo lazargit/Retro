3939bd5887468d68044188cfbbab99a7accd2e9c
Shamil Lazar <shamil.lazar@gmail.com>
2018-08-27 14:07:56 +0200

    Update UrlManager.java

--- data/src/main/java/com/shamildev/retro/data/net/UrlManager.java
+++ data/src/main/java/com/shamildev/retro/data/net/UrlManager.java
@@ -7,10 +7,5 @@ package com.shamildev.retro.data.net;
 public class UrlManager {
 
 
-    public static final String BASE_URL_MDB = "https://api.themoviedb.org/";
-    public static final String PUBLIC_KEY_MDB = "96306bc3cc12ed9ef756ba9a85628586";
-
-    public static final String REPOS = "/users/{user}/repos";
-    public static final String API_HOST = "https://api.github.com";
 
 }
