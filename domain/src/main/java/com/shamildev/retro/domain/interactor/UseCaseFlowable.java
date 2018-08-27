f4690ac2ff62b67e7a16b5563231d950e3f81976
Shamil Lazar <shamil.lazar@gmail.com>
2018-08-27 13:58:58 +0200

    Update UseCaseFlowable.java

--- domain/src/main/java/com/shamildev/retro/domain/interactor/UseCaseFlowable.java
+++ domain/src/main/java/com/shamildev/retro/domain/interactor/UseCaseFlowable.java
@@ -3,13 +3,7 @@ package com.shamildev.retro.domain.interactor;
 import io.reactivex.Flowable;
 import io.reactivex.Observable;
 
-/**
- * Use cases are interactors in terms of "clean architecture", which encapsulate a focused unit of
- * work. These do not, by themselves, determine where (what thread) the work will be done.
- *
- * @param <K> the type of the input passed as a parameter to {@link #execute(K)}
- * @param <V> the type of the item emitted by the {@link Observable} returned by {@link #execute(V)}
- */
+
 public interface UseCaseFlowable<K, V> {
 
     Flowable<V> execute(K params);
